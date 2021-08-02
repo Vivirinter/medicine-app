b599b7b76fb6bacd16a2fe4304156891f50d2721
Vivirinter <vivirequesty@gmail.com>
2021-08-02 23:58:46 +0300

    the main

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:medicine_app/pages/splash_screen.dart';
+import 'package:medicine_app/theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: greenColor),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color greenColor = Color(0xff4fc65b);
+Color whiteColor = Color(0xffffffff);
+Color blackColor = Color(0xff000000);
+Color greyBoldColor = Color(0xff696060);
+Color greyLightColor = Color(0xFFADADAD);
+Color grey35Color = Color(0x59989898);
+
+TextStyle lightTextStyle = GoogleFonts.firaSans().copyWith(fontWeight: FontWeight.w300);
+TextStyle regulerTextStyle = GoogleFonts.firaSans().copyWith(fontWeight: FontWeight.w400);
+TextStyle mediumTextStyle = GoogleFonts.firaSans().copyWith(fontWeight: FontWeight.w500);
+TextStyle boldTextStyle = GoogleFonts.firaSans().copyWith(fontWeight: FontWeight.w700);
+
+
